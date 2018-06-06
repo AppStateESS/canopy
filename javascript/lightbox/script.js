a3fd62c9d3c63e3f64416034a1314e230e3d1b2c
Matthew McNaney <mcnaney@gmail.com>
2018-06-06 12:19:29 -0400

    Removed lightbox from Javascript directory

--- javascript/lightbox/script.js
+++
@@ -1,1 +0,0 @@
-function initLightbox(tag) {$(tag).lightBox({imageLoading: lbImageLoading,imageBtnClose: lbImageBtnClose,imageBtnPrev: lbImageBtnPrev,imageBtnNext: lbImageBtnNext,imageBlank: lbImageBlank,txtImage: lbTxtImage,txtOf: lbTxtOf})};

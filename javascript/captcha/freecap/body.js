9a96136dd8e28191b36edbafdf0b278cbff1ef31
Matthew McNaney <mcnaney@gmail.com>
2018-06-06 12:21:59 -0400

    Removed freecap captcha

--- javascript/captcha/freecap/body.js
+++
@@ -1,6 +0,0 @@
-<a href="#" onClick="this.blur();new_freecap();return false;"><img src="{source_http}javascript/captcha/freecap/freecap.php?s={sid}" id="freecap"></a>
-<p>
-<label for="freecap-captcha" id="freecap-captcha-label" class="textfield-label">{question}</label><br />
-<input type="text" value="" title="{question}" id="freecap-captcha" name="captcha"/>
-</p>
-{warning}

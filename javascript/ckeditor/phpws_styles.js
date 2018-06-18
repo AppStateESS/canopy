85ae70d005e0a212684388a1fe6e901292c5b95c
Matthew McNaney <mcnaney@gmail.com>
2018-06-18 10:55:42 -0400

    Altered ckeditor config to work better with bootstrap.

--- javascript/ckeditor/phpws_styles.js
+++ javascript/ckeditor/phpws_styles.js
@@ -1,15 +1,12 @@
 CKEDITOR.stylesSet.add( 'phpws',
 [
    // Block-level styles
-   { name : 'Pull image right', element : 'img', attributes : { 'class' : 'float-right' }, styles : { 'margin' : '0 0 1em 1em' } },
-   { name : 'Pull image left', element : 'img', attributes : { 'class' : 'pull-left' }, styles : { 'margin' : '0 1em 1em 0' } },
-   { name : 'Inset', element : 'p', attributes : { 'class' : 'well' } },
-   { name : 'Smaller', element : 'small'},
-   { name : 'Mark', element : 'mark'},
-   { name : 'Offsite icon', element: 'i', attributes : {'class': 'fa fa-external-link'}},
-   { name : 'Red alert', element: 'p', attributes : {'class': 'alert alert-danger'}},
-   { name : 'Green alert', element: 'p', attributes : {'class': 'alert alert-success'}},
-   { name : 'Blue alert', element: 'p', attributes : {'class': 'alert alert-info'}},
+   { name : 'Float image right', element : 'img', attributes : { 'class' : 'float-right ml-1 mb-1' }},
+   { name : 'Float image left', element : 'img', attributes : { 'class' : 'float-left mr-1 mb-1' }},
+   { name : 'Inset', element : 'p', attributes : { 'class' : 'alert alert-dark' } },
+   { name : 'Red alert', element: 'div', attributes : {'class': 'alert alert-danger'}},
+   { name : 'Green alert', element: 'div', attributes : {'class': 'alert alert-success'}},
+   { name : 'Blue alert', element: 'div', attributes : {'class': 'alert alert-info'}},
    { name : 'Striped Table', element: 'table', attributes : {'class': 'table table-striped'}},
    { name : 'Bordered Table', element: 'table', attributes : {'class': 'table table-bordered'}},
    { name : 'Striped, Bordered Table', element: 'table', attributes : {'class': 'table table-bordered table-striped'}}

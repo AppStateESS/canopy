b53df4063c84f0a01903bf5eaee47d2c3b4e7051
Matt McNaney <mcnaneym@appstate.edu>
2018-11-09 10:43:25 -0500

    Code formatting changed

--- mod/layout/javascript/move_boxes.js
+++ mod/layout/javascript/move_boxes.js
@@ -1,39 +1,37 @@
-$(document).ready(function() {
-    $('#move-boxes').click(function() {
-        fillModal();
-        $('#box-move').modal('show');
-    });
-    $('#box-move').on('hidden.bs.modal', function (e) {
-        window.location.reload();
-    });
+$(document).ready(function () {
+  $('#move-boxes').click(function () {
+    fillModal();
+    $('#box-move').modal('show');
+  });
+  $('#box-move').on('hidden.bs.modal', function (e) {
+    window.location.reload();
+  });
 });
 
-function fillModal()
-{
-    $.get('index.php', {
-        module: 'layout',
-        action: 'admin',
-        command: 'boxMoveForm'
-    }).done(function(data) {
-        $('#box-move .modal-body').html(data);
-        setChange();
-    });
+function fillModal() {
+  $.get('index.php', {
+    module: 'layout',
+    action: 'admin',
+    command: 'boxMoveForm'
+  }).done(function (data) {
+    $('#box-move .modal-body').html(data);
+    setChange();
+  });
 }
 
-function setChange()
-{
-    $('.move-box').change(function(e) {
-        var value = e.target.value;
-        var id = $(e.target).data('id');
+function setChange() {
+  $('.move-box').change(function (e) {
+    var value = e.target.value;
+    var id = $(e.target).data('id');
 
-        $.get('index.php', {
-            module: 'layout',
-            action: 'admin',
-            command: 'moveBox',
-            box_source: id,
-            box_dest: value
-        }).success(function() {
-            fillModal();
-        });
+    $.get('index.php', {
+      module: 'layout',
+      action: 'admin',
+      command: 'moveBox',
+      box_source: id,
+      box_dest: value
+    }).done(function () {
+      fillModal();
     });
+  });
 }

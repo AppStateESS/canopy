2194e80af89fcb243bdaf381e1ff1d8c2e3b52cd
Matt McNaney <mcnaneym@appstate.edu>
2019-01-29 08:05:38 -0500

    Changed forward logic.

--- mod/access/inc/runtime.php
+++ mod/access/inc/runtime.php
@@ -7,7 +7,7 @@
 
 // If no one else has set $_REQUEST['module'] by this point and Core wants us to 
 // forward, 404.
-if(isset($GLOBALS['Forward']) && !array_key_exists('module', $_REQUEST)) {
+if(!empty($GLOBALS['Forward']) && !array_key_exists('module', $_REQUEST)) {
     \phpws\PHPWS_Core::errorPage(404);
 }
 

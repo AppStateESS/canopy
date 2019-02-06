aef277fbd0e61e2e29af2b8c8339f9cb331fa504
Ted <eberhardtm@appstate.edu>
2019-02-06 12:56:44 -0500

    Fix logout script

--- secure/logout.php
+++ secure/logout.php
@@ -5,40 +5,26 @@ session_start();
 unset($_SESSION);
 session_destroy();
 
-
 // Shibboleth local logout is always relative to the root
 $shiblocallogout = 'https://' . $_SERVER['HTTP_HOST'] . '/Shibboleth.sso/Logout';
+$shiblogout = $_SERVER['HTTP_SHIB_LOGOUTURL'];
+require_once('../config/core/config.php');
+require_once('../config/core/config.php');
+require_once PHPWS_SOURCE_DIR . 'src/Server.php';
 
-// Our destination, however, not necessarily so figure it out
 $parts = explode('/', $_SERVER['SCRIPT_NAME']);
 while(array_pop($parts) != 'secure');
-$destination = 'http://' . $_SERVER['HTTP_HOST'] . implode('/', $parts);
+$return_url = 'http://' . $_SERVER['HTTP_HOST'] . implode('/', $parts);
+?>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="2;url=<?php echo $return_url; ?>" />
+    </head>
+    <body>
+    <p>Logging you out...</p>
+  <p><a href="<?php echo $return_url; ?>">If you are not redirected automatically, please click this link.</a></p>
+      <iframe style="display: none" src="<?php echo $shiblogout; ?>"><p>Logging You Out...</p></iframe>
+      <iframe style="display: none" src="<?php echo $shiblocallogout; ?>"><p>Logging You Out...</p></iframe>
 
-if(isset($_SERVER['AUTH_TYPE'])) {
-    if(strtolower($_SERVER['AUTH_TYPE']) == 'shibboleth') {
-?>
-<html>
-    <head>
-        <meta http-equiv="refresh" content="2;url=<?php echo $destination; ?>" />
-    </head>
-    <body>
-    <p>Logging you out...</p>
-    <p><a href="<?php echo $destination; ?>">If you are not redirected automatically, please click this link.</a></p>
-    <iframe style="display: none" src="<?php echo $_SERVER['HTTP_SHIB_LOGOUTURL']; ?>?return_url=<?php echo $shiblocallogout?>"><p>Logging You Out...</p></iframe>
     </body>
 </html>
-<?php
-    } else if(strtolower($_SERVER['AUTH_TYPE']) == 'cosign') {
-?>
-<html>
-    <head>
-        <meta http-equiv="refresh" content="2; url=<?php echo COSIGN_LOGOUT_URL; ?>" />
-    </head>
-    <body>
-    <p>Logging you out...</p>
-    <p><a href="<?php echo COSIGN_LOGOUT_URL; ?>">If you are not redirected automatically, please click this link.</a></p>
-    </body>
-</html>
-<?php
-    }
-}

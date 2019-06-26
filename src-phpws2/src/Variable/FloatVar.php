59ae50c49ec774974f77051dd5186fc7bf364f61
Matt McNaney <mcnaneym@appstate.edu>
2019-06-26 07:17:46 -0400

    Added ability to add precision to float
    Creating a table was problematic.

--- src-phpws2/src/Variable/FloatVar.php
+++ src-phpws2/src/Variable/FloatVar.php
@@ -11,13 +11,16 @@ namespace phpws2\Variable;
  */
 class FloatVar extends \phpws2\Variable
 {
+
     /**
-    /* We use decimal instead of float as it is used
-    /* in mysql and pgsql.
+      /* We use decimal instead of float as it is used
+      /* in mysql and pgsql.
      * If you must use float, overwrite the column_type
      * @var string
      */
     protected $column_type = 'decimal';
+    protected $digits = 10;
+    protected $decimalPoint = 1;
 
     /**
      * Checks to see if value is a float.
@@ -26,10 +29,17 @@ class FloatVar extends \phpws2\Variable
      */
     protected function verifyValue($value)
     {
-        if (!is_float($value)) {
+        
+        if (!is_numeric($value) || !is_float((float)$value)) {
             throw new \Exception('Value is not a float');
         }
         return true;
+    }
+
+    public function setPrecision(int $digits, int $decimalPoint)
+    {
+        $this->digits = $digits;
+        $this->decimalPoint = $decimalPoint;
     }
 
     /**
@@ -39,6 +49,27 @@ class FloatVar extends \phpws2\Variable
     public function __toString()
     {
         return (string) $this->get();
+    }
+
+    /**
+     *
+     * @param \phpws2\Database\Table $table
+     * @return \phpws2\Database\Datatype
+     */
+    public function loadDataType(\phpws2\Database\Table $table)
+    {
+        $size = $this->digits . ',' . $this->decimalPoint;
+        $dt = parent::loadDataType($table);
+        $dt->setSize($size);
+        $default = "";
+        if ($dt->getDefault()->get() == 0) {
+            for ($i=0; $i < $this->decimalPoint; $i++) {
+                $default .= '0';
+            }
+            $decimals = "0.$default";
+            $dt->setDefault($decimals);
+        }
+        return $dt;
     }
 
 }

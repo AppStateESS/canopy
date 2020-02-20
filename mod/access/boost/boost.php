ead96507a5fff3a2d01822c5cd4c8445b502eb22
Matt McNaney <mcnaneym@appstate.edu>
2020-02-20 13:38:43 -0500

    Fixed access pulling a blank url shortcut

--- mod/access/boost/boost.php
+++ mod/access/boost/boost.php
@@ -1,14 +1,13 @@
 <?php
+
 /**
  * @author Matthew McNaney
- 
  */
-
-$proper_name  = 'Access';
-$version      = '1.3.0';
-$import_sql   = TRUE;
-$about        = TRUE;
-$file_dir     = TRUE; // required for backup access files
-$priority     = 15;
-$dependency   = true;
+$proper_name = 'Access';
+$version = '1.3.1';
+$import_sql = TRUE;
+$about = TRUE;
+$file_dir = TRUE; // required for backup access files
+$priority = 15;
+$dependency = true;
 
